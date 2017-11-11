ce9cde1cfdbb9b3ea8cd481249f5dbc7fdc9ec64
Lex Flagel <flag0010@gmail.com>
2017-11-10 21:15:01 -0600

    Update introgression.conv.net.py

--- introgression/introgression.conv.net.py
+++ introgression/introgression.conv.net.py
@@ -1,6 +1,5 @@
 import numpy as np
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv1D, MaxPooling1D, AveragePooling1D

ac539cbeff691a7e668136e8650d24a02990ccae
Lex Flagel <flag0010@gmail.com>
2018-02-09 09:23:26 -0600

    Update genet.data.matrix.prep.tricks.py

--- data_prep_tricks/genet.data.matrix.prep.tricks.py
+++ data_prep_tricks/genet.data.matrix.prep.tricks.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import NearestNeighbors
 def sort_min_diff(amat):
     '''this function takes in a SNP matrix with indv on rows and returns the same matrix with indvs sorted by genetic similarity.
-    this problem is NP-hard, so here we use a nearest neighbors approx.  it's not perfect, but it's fast and generally performs ok.
+    this problem is NP, so here we use a nearest neighbors approx.  it's not perfect, but it's fast and generally performs ok.
     assumes your input matrix is a numpy array'''
     mb = NearestNeighbors(len(amat), metric='manhattan').fit(amat)
     v = mb.kneighbors(amat)

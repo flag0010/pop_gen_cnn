212ac029afe4971141d9735af4520582d6eb44ca
flag0010 <flag0010@gmail.com>
2018-02-21 09:10:42 -0600

    reorg again

--- introgression/extract.big.data.set.py
+++ introgression/extract.big.data.set.py
@@ -1,12 +1,3 @@
-##!/bin/bash -l
-##PBS -l nodes=1:ppn=2,mem=100gb,walltime=1:30:00
-##PBS -e error.txt
-##PBS -o out.txt
-
-#cd /home/mcgaughs/flag0010/FILET/
-
-#python extract.big.data.set.py
-
 import numpy as np
 from random import shuffle
 import gzip

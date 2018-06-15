7be845444cd037ce36a1944dc9846dcd8c84490d
Lex Flagel <flag0010@gmail.com>
2018-06-15 08:13:00 -0500

    Update train.phased.mergenet.PYTHON3.py

--- historical_recombination/phased/train.phased.mergenet.PYTHON3.py
+++ historical_recombination/phased/train.phased.mergenet.PYTHON3.py
@@ -14,7 +14,7 @@ def rmse(x,y):
     return np.sqrt(np.mean((x-y)**2))
 
 #load and prep data
-a = np.load('./dataset/ld.data.npz' , encoding='latin1')
+a = np.load('../dataset/ld.data.npz' , encoding='latin1')
 
 postrain, ytest, ytrain, xtest, xtrain, postest = [a[i] for i in ['postrain', 'ytest', 'ytrain', 'xtest', 'xtrain', 'postest']]
 
@@ -24,7 +24,6 @@ xtest = sequence.pad_sequences(xtest, maxlen=418, padding='post')
 
 postrain = sequence.pad_sequences(postrain, maxlen=418, padding='post', value=-1., dtype='float32')
 postest = sequence.pad_sequences(postest, maxlen=418, padding='post', value=-1., dtype='float32')
-
 
 #prep y data
 ytrain_rho, ytest_rho = np.array([i[1] for i in ytrain]), np.array([i[1] for i in ytest])
@@ -42,34 +41,35 @@ plt.hist(ytest_rho_log_centered)
 plt.show()
 print (mean_test, mean_train) #mean train is important, it was 4.78757605959
 
-
 batch_size = 32
+l2_lambda = 0.0001
 
 b1 = Sequential()
 b1.add(Conv1D(1250, kernel_size=2,
                  activation='relu',
+                 kernel_regularizer=keras.regularizers.l2(l2_lambda),
                  input_shape=(xtest.shape[1], xtest.shape[2])))
-b1.add(Conv1D(256, kernel_size=2, activation='relu'))
+b1.add(Conv1D(256, kernel_size=2, kernel_regularizer=keras.regularizers.l2(l2_lambda), activation='relu'))
 b1.add(AveragePooling1D(pool_size=2))
 b1.add(Dropout(0.25))
-b1.add(Conv1D(256, kernel_size=2, activation='relu'))
+b1.add(Conv1D(256, kernel_size=2, kernel_regularizer=keras.regularizers.l2(l2_lambda), activation='relu'))
 b1.add(AveragePooling1D(pool_size=2))
 b1.add(Dropout(0.25))
 b1.add(Flatten())
 
 b2 = Sequential()
-b2.add(Dense(64, input_shape = (418,), activation='relu'))
+b2.add(Dense(64, input_shape = (418,), kernel_regularizer=keras.regularizers.l2(l2_lambda), activation='relu'))
 b2.add(Dropout(0.1))
 
 model = Sequential()
 #model.add([b1, b2])
 #model = keras.layers.concatenate([b1,b2])
 model.add(Merge([b1, b2], mode = 'concat'))
-model.add(Dense(256, activation='relu'))
+model.add(Dense(256, kernel_regularizer=keras.regularizers.l2(l2_lambda), activation='relu'))
 model.add(Dense(1, kernel_initializer='normal'))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-for III in range(5):
+for III in range(25):
     if III < 1: model.summary()
 
     model.fit([xtrain, postrain], ytrain_rho_log_centered, batch_size=batch_size,
@@ -83,40 +83,9 @@ for III in range(5):
     plt.show()
     print (III, rsquare(np.exp(ytest_rho_log_centered+mean_train), np.exp(pred)+mean_train), 
            rmse(np.exp(ytest_rho_log_centered+mean_train), np.exp(pred+mean_train)))
-    
+    if rmse(np.exp(ytest_rho_log_centered+mean_train), np.exp(pred+mean_train)) < 210: break
+ 
 
 j = model.to_json()
 print(j)  #need to save this weith weights file to run on validation
-model.save_weights('merge.mod.weights')
-
-#from matplotlib import pyplot as plt
-
-#pred = np.array([np.exp(ii[0]+mean_test) for ii in model.predict([xtest,postest])])
-#thetas=[i[0] for i in ytest]
-
-#print(map(len, (thetas, pred, ytest_rho)))
-
-#q = {}
-#for i,j,k in zip(thetas, pred, ytest_rho):
-#    if i not in q: q[i] = []
-#    q[i].append((j,k))
-#for i in q: print( i, len(q[i]) )
-
-#idx=1
-#for i in sorted(q):
-#    plt.subplot(2,3,idx)
-#    pred, real = [n[0] for n in q[i]], [n[1] for n in q[i]]
-#    plt.scatter(real, pred, alpha=.3)
-#    r = rsquare(np.array(real), np.array(pred))
-#    plt.title('theta = '+str(i)+' r2:'+str(round(r,1)))
-#    idx+=1
-#plt.show()
-
-
-#d = []
-#for i in sorted(q):
-#    resid = [p-r for p,r in q[i]]
-#    d.append(resid)
-#plt.boxplot(d)
-#plt.xticks(range(1,6), map(str, sorted(q)))
-#plt.show()
+model.save_weights('regularized.merge.mod.weights')

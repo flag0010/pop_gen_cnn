ebd95cc9d6ced0badbdd872ef3a82507426d666a
Lex Flagel <flag0010@gmail.com>
2018-07-22 11:00:47 -0500

    Create sort.min.diff.py

---
+++ sort.min.diff.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import NearestNeighbors
+def sort_min_diff(amat):
+    '''this function takes in a SNP matrix with indv on rows and returns the same matrix with indvs sorted by genetic similarity.
+    this problem is NP, so here we use a nearest neighbors approx.  it's not perfect, but it's fast and generally performs ok.
+    assumes your input matrix is a numpy array'''
+    mb = NearestNeighbors(len(amat), metric='manhattan').fit(amat)
+    v = mb.kneighbors(amat)
+    smallest = np.argmin(v[0].sum(axis=1))
+    return amat[v[1][smallest]]

e56333ec01c8860da3c7d865d7c7866094e2cc6b
Lex Flagel <flag0010@gmail.com>
2019-02-19 20:48:23 -0600

    commented out plotting junk

--- theta/extract.data.set.for.training.py
+++ theta/extract.data.set.for.training.py
@@ -46,42 +46,42 @@ xtrain, xtest = x[1000:], x[:1000]
 ytrain, ytest = y[1000:], y[:1000]
 np.savez_compressed('theta_sim.npz', xtest=xtest, xtrain=xtrain, ytest=ytest, ytrain=ytrain)#, fin_xtest=fin_xtest, fin_ytest=fin_ytest)
 
-print x[1].shape
+#print x[1].shape
 
-def pi(x):
-    v = []
-    d = choose(x[0].shape[1], 2) ##
-    for i in x:
-        s = i.sum(axis=1)
-        k = []
-        for j in s:
-            n = choose(j, 2)+choose(i.shape[1]-j, 2)
-            k.append((d-n)*d**-1)
-        v.append(sum(k))
-    return np.array(v)
+#def pi(x):
+#    v = []
+#    d = choose(x[0].shape[1], 2) ##
+#    for i in x:
+#        s = i.sum(axis=1)
+#        k = []
+#        for j in s:
+#            n = choose(j, 2)+choose(i.shape[1]-j, 2)
+#            k.append((d-n)*d**-1)
+#        v.append(sum(k))
+#    return np.array(v)
 
-def theta(x):
-    v = []
-    a = sum([i**-1 for i in range(1, x[0].shape[1])])
-    print a
-    for i in x:
-        S = i.shape[0]
-        print S
-        v.append(S/a)
-    return np.array(v)
+#def theta(x):
+#    v = []
+#    a = sum([i**-1 for i in range(1, x[0].shape[1])])
+#    print a
+#    for i in x:
+#        S = i.shape[0]
+#        print S
+#        v.append(S/a)
+#    return np.array(v)
 
-y = np.array(y)
-pix = pi(x)
-thetax = theta(x)
-print np.mean((pix-y)**2)**.5
-print np.mean((thetax-y)**2)**.5
-ensemble = (pix+thetax)/2.
-print np.mean((ensemble-y)**2)**.5
+#y = np.array(y)
+#pix = pi(x)
+#thetax = theta(x)
+#print np.mean((pix-y)**2)**.5
+#print np.mean((thetax-y)**2)**.5
+#ensemble = (pix+thetax)/2.
+#print np.mean((ensemble-y)**2)**.5
 
 
-from matplotlib import pyplot as plt
-plt.subplot(1,2,1)
-plt.scatter(y, pix, alpha=.4)
-plt.subplot(1,2,2)
-plt.scatter(y, thetax, alpha=.4)
-plt.show()
+#from matplotlib import pyplot as plt
+#plt.subplot(1,2,1)
+#plt.scatter(y, pix, alpha=.4)
+#plt.subplot(1,2,2)
+#plt.scatter(y, thetax, alpha=.4)
+#plt.show()
